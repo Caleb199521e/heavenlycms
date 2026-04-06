83f215cc89cc0237da52f2690930d8b0ac57345f
CALEB AMPONG <campong004@st.ug.edu.gh>
2026-04-06 19:46:42 +0000

    feat(cors): add 'x-request-id' to allowed headers for CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL || '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-request-id'],
 }));
 
 // Body parsing with size limits
